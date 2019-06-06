ab0ee3b5f4a2bd7623f931368b7835e82ea5ea44
Paul Rail <paul.a.rail@gmail.com>
2019-06-06 13:01:33 -0400

    onChange/onPress setState button

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Dimensions, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+class App extends React.Component {
+
+  state = {
+    text: "",
+    todo: ""
+  }
+
+  addTodo = () => {
+    this.setState({todo: this.state.text});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>New Udemy course</Text>
+        <TextInput
+          style={styles.inputStyle}
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Button
+          style={styles.buttonStyles}
+          title="Add Todo"
+          onPress={this.addTodo}
+        />
+        <Text>{this.state.todo}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +45,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputStyle: {
+    height: 40,
+    width: 0.8*Dimensions.get('window').width, 
+    borderColor: "green",
+    borderWidth: 1,
+    marginBottom: 20
+  },
+  buttonStyles: {
+    marginTop: 20,
+    paddingTop: 20
+  }
 });
+
+
+export default App;
